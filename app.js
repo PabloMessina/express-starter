42f4706264edc785a7e6c5c33b3b20896d9bc63a
PabloMessina <pamessina@uc.cl>
2016-04-06 18:24:54 -0300

    finish express backend

--- app.js
+++ app.js
@@ -1,12 +1,63 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const app = express()
-app.use(morgan('tiny'))
+// set up and connect to mongodb database
+const mongoose = require('mongoose');
+const db = mongoose.connect('mongodb://localhost/test');
+//message schema
+const MessageSchema = mongoose.Schema({
+    content: String,
+    date: Date
+});
+//message model
+const Message = mongoose.model('Message',MessageSchema);
+
+// setup app
+const app = express();
+app.use(cors());
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   // Send JSON
-  res.send({ status: 'on' })
-})
+  res.send({ status: 'on' });
+});
 
-module.exports = app
+//return list of all messages as JSON
+app.get('/messages', (req,res) => {
+    Message.find().lean(true).exec( (err,messages) => {
+        console.log("=========================");
+        console.log("err = ", err);
+        console.log("messages = ",messages);
+        res.send({ messages: messages });
+    });    
+});
+
+//check for non-empty string
+function isNonEmptyString(input) {
+    return (input instanceof String || typeof input === 'string') && input !== "";
+}
+
+//save received message asynchronously into mongoDB
+app.post('/messages', (req, res) => {
+    const received_message = req.body.message;
+    if (isNonEmptyString(received_message)) {
+        const message = new Message();
+        message.content = received_message;
+        message.date = new Date();
+        console.log("received_message = ",received_message);
+        message.save(()=> {
+            res.send(req.body);
+            console.log("message saved!!");
+        });
+    }
+});
+
+module.exports = app;
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 
-const app = require('./app')
+const app = require('./app');
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(3000, '0.0.0.0',() => {
+  console.log('Example app listening on port 3000!');
+});
 console.log('Setup complete');
